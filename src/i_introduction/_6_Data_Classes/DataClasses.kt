ec9e89cb001f3cff9bb2c029a6ff0954f4c322d4
kozake <sh.kozake@gmail.com>
2016-04-20 07:07:51 +0900

    task6

--- src/i_introduction/_6_Data_Classes/DataClasses.kt
+++ src/i_introduction/_6_Data_Classes/DataClasses.kt
@@ -5,10 +5,11 @@ import util.doc6
 
 fun todoTask6(): Nothing = TODO(
     """
-        Convert 'JavaCode6.Person' class to Kotlin.
-        Then add a modifier `data` to the resulting class.
-        This annotation means the compiler will generate a bunch of useful methods in this class: `equals`/`hashCode`, `toString` and some others.
-        The `task6` function should return a list of persons.
+        'JavaCode6.Person' クラスをKotlinで書き換えてください。
+        そして、そのクラスにdataを追加してください。
+        このアノーテーションは、コンパイルすると`equals`/`hashCode`, `toString`などの
+        有用なメソッド群を生成します。
+        `task6`の関数はPersonのリストを返します。
     """,
     documentation = doc6(),
     references = { JavaCode6.Person("Alice", 29) }
