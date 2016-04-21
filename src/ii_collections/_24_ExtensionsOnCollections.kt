7239ff2e258e9ca2c3e36fa4def6ce0d591d0138
kozake <sh.kozake@gmail.com>
2016-04-22 07:43:53 +0900

    task24

--- src/ii_collections/_24_ExtensionsOnCollections.kt
+++ src/ii_collections/_24_ExtensionsOnCollections.kt
@@ -5,8 +5,8 @@ import util.TODO
 fun todoTask24(): Nothing = TODO(
     """
         Task 24.
-        The function should behave the same as '_24_JavaCode.doSomethingStrangeWithCollection'
-        Replace all invocations of 'todoTask24()' with the appropriate code.
+        この関数は'_24_JavaCode.doSomethingStrangeWithCollection'と同じように動作する必要があります。
+        適切なコードで'todoTask24()'のすべての呼び出しを交換してください。
     """,
         references = { c: Collection<String> -> _24_JavaCode().doSomethingStrangeWithCollection(c) }
 )
